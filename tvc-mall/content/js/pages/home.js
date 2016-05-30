62aca4752e8fa97252eff976b9ddd40f220d9b84
youya625 <rumble625@foxmail.com>
2016-05-30 22:10:57 +0800

    554545

---
+++ tvc-mall/content/js/pages/home.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    //轮播图
+    $('#banner').carousel();
+    $('#featured-banner').carousel({
+        interval: false
+    });
+    $('#compare-banner').carousel({
+        interval: false
+    });
+
+    //倒计时
+    $(".s-time").countdowntimer({
+        hours: 3,
+        minutes: 10,
+        seconds: 10,
+        size: "lg",
+    });
+
+    //延迟加载
+    $("img.lazy").lazyload({
+        effect: "fadeIn"
+    });
+    setInterval(function () { $(window).scroll(); }, 1000)
+})
---
+++ tvc-mall/content/js/pages/productDetail.js
@@ -0,0 +1,35 @@
+﻿$(function () {
+
+    $('#images-slider').carousel({
+        interval: false
+    });
+
+    //选项卡
+    $('#info-tab a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+
+    $(".tab-wrap").pin({
+        containerSelector: ".info-right"
+    })
+
+    //放大镜
+    $('.jqzoom').jqzoom({
+        zoomType: 'standard',
+        lens: true,
+        preloadImages: false,
+        alwaysOn: false,
+    });
+
+    //延迟加载
+    $("img.lazy").lazyload({
+        effect: "fadeIn"
+    });
+
+    //checkbox初始化
+    $("input[type='checkbox']").iCheck({
+        checkboxClass: 'icheckbox_flat-orange',
+        radioClass: 'iradio_flat-orange'
+    });
+})
